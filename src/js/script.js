9ccbbe47c7e046664d9e9b5b428be5d5f7e4eb1b
Mukhammad <spacelive.company@gmail.com>
2021-09-21 12:47:49 +0300

    Plus. day03: 4 section desctop + mobil done. Slick slider JQ.

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,19 @@
-$(function() {
-    $('.carousel__slider').slick({
-        infinite: true,
-        speed: 300,
-        fade: true,
-        cssEase: 'linear',
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/chevron\ left\ solid.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/chevron\ right\ solid.svg"></img></button>',
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinity: true,
+        speed: 700,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="carousel__prev slick-prev"><img src="icons/carousel/chevron left solid.svg"></button>',
+        nextArrow: '<button type="button" class="carousel__next slick-next"><img src="icons/carousel/chevron right solid.svg"></button>',
         responsive: [
             {
-                breakpoint: 992,
-                settings: {
-                    arrows: false
-                }
-            }
+              breakpoint: 992,
+              settings: {
+                  arrows: false,
+              }
+            },
         ]
     });
-
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-      });
-
-    function toggleSlide(className) {
-        $(className).each(function(i) {
-            $(this).on('click', function(e) {
-                e.preventDefault();
-                $('.card__content').eq(i).toggleClass('card__content_active');
-                $('.card__list').eq(i).toggleClass('card__list_active');
-            })
-        })
-    }
-
-    toggleSlide('.card__link');
-    toggleSlide('.card__list-link');
-})
+  });
